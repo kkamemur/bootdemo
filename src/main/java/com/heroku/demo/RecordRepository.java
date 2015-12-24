88b8614a0a18e96f427b07e79debc5a7cb80efd7
keisuke@platinum <ksk.k@icloud.com>
2015-12-24 18:49:31 +0900

    simplify

--- src/main/java/com/heroku/demo/RecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.heroku.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//@Repository
-public interface RecordRepository extends JpaRepository<Record, Long> {
-}
